468b8e517a3f7c39c08595d4b06a38b2b559dc75
Ryan <rburgess@evernote.com>
2014-02-23 18:55:28 -0800

    Release v0.0.5
    Change-Id: I1291b4bb0f2ba60314521248cb8c209f1340fb52

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,15 @@ module.exports = function(grunt) {
 		bump: {
 		  options: {
 		  	files: ['package.json', 'bower.json'],
-		    updateConfigs: [],
+		    updateConfigs: ["pkg"],
 		    commit: true,
 		    commitMessage: 'Release v%VERSION%',
 		    commitFiles: ['package.json'], // '-a' for all files
 		    createTag: true,
 		    tagName: 'v%VERSION%',
-		    tagMessage: 'Version %VERSION%'
+		    tagMessage: 'Version %VERSION%',
+        	commitFiles: ["-a"],
+        	push: false
 		  }
 		}
 	});
