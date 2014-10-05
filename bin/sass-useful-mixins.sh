45eb0aeb338c5d7437c6be1d8655c723ede480df
Ryan <rburgess@evernote.com>
2014-10-05 10:10:12 -0700

    update package
    Change-Id: If9aab27f281fb0c4f267c806555a8b1027bbb01d

---
+++ bin/sass-useful-mixins.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+git clone https://github.com/ryanburgess/SASS-Useful-Mixins.git
+
+rm -r -f README.md package.json
+
+mv /* .
+
+echo ''
+echo "Sass Useful Mixins have been successfully added to your project"
+echo ''
