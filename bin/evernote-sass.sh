af261589efc9037919c66cdc815f89b8f1069003
Ryan <rburgess@evernote.com>
2014-10-05 10:05:55 -0700

    update package
    Change-Id: Ibe5a2f30ecf6d0817621e158c1bed155d31da29f

---
+++ bin/evernote-sass.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+git clone https://github.com/evernote/sass-build-structure.git
+
+rm -r -f README.md package.json
+
+mv /* .
+
+echo ''
+echo "The Evernote Sass build has been successfully added to your project"
+echo ''
